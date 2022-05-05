1d7a45e91cdf8d5079992b02a3990fb24f0331aa
kuangqijie <635389271@qq.com>
2022-05-05 11:16:22 +0800

    5.5

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -7,28 +7,9 @@ export default {
     };
   },
   created() {
-    setInterval(() => {
-      this.count++;
-    }, 1000);
+
   },
   mounted() {
-    new Swiper(".mySwiper", {
-      direction: "vertical",
-      slidesPerView: "auto",
-      freeMode: true,
-      scrollbar: {
-        el: ".mySwiper .swiper-scrollbar",
-      },
-      mousewheel: true,
-    });
 
-    new Swiper(".mySwiper2", {
-      slidesPerView: "auto",
-      freeMode: true,
-      scrollbar: {
-        el: "#swiper-scrollbar2",
-      },
-      mousewheel: true,
-    });
   },
 };
